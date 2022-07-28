0820466e0c82a5938cf7a19ebe8eb1202eb8933d
Samuel Orji <samuelorji53@gmail.com>
2022-07-28 14:06:04 -0500

    DevOps lab

--- server/index.js
+++ server/index.js
@@ -1,13 +1,26 @@
 const express = require('express')
 const path = require('path')
-
 const app = express()
+
+//devops lab
+require('dotenv').config()
+app.use(express.json())
+
 
 app.use(express.static(path.join(__dirname, "/../public")))
 
 app.get('/', function(req,res) {
     res.sendFile(path.resolve('public/index.html'))
 })
+
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar ({
+    accessToken: process.env.ROLLBAR_ACCESS_TOKEN,
+    captureUncaught: true,
+    captureUnhandledRejections: true,
+})
+
+rollbar.log('Hello world!')
 
 const port = process.env.PORT || 4006
 
